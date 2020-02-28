340098c1d9a770139a20b99f541d221832373d0b
Ashwin Sajiv <sajivashwin@gmail.com>
2020-02-28 14:07:38 +1100

    Create sysl.rb

---
+++ sysl.rb
@@ -0,0 +1,10 @@
+class Sysl < Formula
+  desc "Sysl (pronounced \"sizzle\") is a system specification language"
+  homepage "https://sysl.io"
+  url "https://github.com/ashwinsajiv/sysl/archive/v0.5.0.tar.gz"
+  sha256 "aeb96e60899f4af978abdd92fff027718f77d07da2d42c22cf6b92b56142487d"
+
+  def install
+    system "ruby", "test.rb"
+  end
+end
